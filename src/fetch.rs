965b5d8f6b8a126c3d262434c8b6f1b3db3f7fe6
ProspectPyxis <ProspectPyxis@users.noreply.github.com>
2022-06-25 13:47:29 +0700

    feat: FetchData struct

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,11 +1,37 @@
-pub fn get_os() -> String {
-    let os = sys_info::os_type().unwrap_or_else(|_| "Unknown".to_string());
-    if os.as_str() == "Linux" {
-        match sys_info::linux_os_release() {
-            Ok(info) => info.id.unwrap_or_else(|| "linux".to_string()),
-            Err(_) => "linux".to_string(),
+use std::io::stdout;
+use crossterm::{
+    queue,
+    style::{Print, PrintStyledContent, Stylize},
+};
+
+pub struct FetchData {
+    label: &'static str,
+    text: String,
+}
+
+impl FetchData {
+    pub fn queue_print(&self) {
+        queue!(
+            stdout(),
+            PrintStyledContent(self.label.bold().cyan()),
+            Print(format!(" {}", self.text)),
+        ).unwrap();
+    }
+}
+
+pub fn get_os() -> FetchData {
+    FetchData {
+        label: "os",
+        text: {
+            let os = sys_info::os_type().unwrap_or_else(|_| "Unknown".to_string());
+            if os.as_str() == "Linux" {
+                match sys_info::linux_os_release() {
+                    Ok(info) => info.id.unwrap_or_else(|| "linux".to_string()),
+                    Err(_) => "linux".to_string(),
+                }
+            } else {
+                os.to_lowercase()
+            }
         }
-    } else {
-        os.to_lowercase()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
 mod fetch;
 
-use std::io::stdout;
-use crossterm::{
-    queue,
-    style::{PrintStyledContent, Stylize},
-};
-
 fn main() {
     let os = fetch::get_os();
 
-    queue!(
-        stdout(),
-        PrintStyledContent(format!("os {}", os).cyan()),
-    ).unwrap();
+    os.queue_print();
 }
 
