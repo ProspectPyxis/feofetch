9999669b8cada78703b9af38d3ebbcf438fe42a0
ProspectPyxis <ProspectPyxis@users.noreply.github.com>
2022-07-21 14:44:22 +0700

    fix: use path.display instead

--- src/config.rs
+++ src/config.rs
@@ -99,7 +99,7 @@ pub fn load_raw_ascii(ascii_path: PathBuf) -> String {
     fs::read_to_string(&ascii_path).unwrap_or_else(|e| {
         panic!(
             "Unable to read ascii file \"{}\": {}",
-            ascii_path.to_string_lossy(),
+            ascii_path.display(),
             e
         )
     })
